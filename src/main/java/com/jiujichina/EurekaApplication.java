4bd5a7cad06841037a47675ceb0eba45823d117f
yonghai <81067920@qq.com>
2017-11-20 15:09:41 +0800

    update  finalname

--- src/main/java/com/jiujichina/EurekaApplication.java
+++ src/main/java/com/jiujichina/EurekaApplication.java
@@ -1,13 +1,20 @@
 package com.jiujichina;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaServer
 @ComponentScan("com.jiujichina")
-public class EurekaApplication {
+public class EurekaApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 	
 	public static void main(String[] args){
 		SpringApplication.run(EurekaApplication.class, args);
